5c6537c41c68242bdce25cde49569eaa14be3c00
David Garcia Ruiz <david.garcia@square1.io>
2022-05-24 16:13:50 +0200

    Initial commit

---
+++ SaytvChatFake/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaytvChatFake
+//
+//  Created by David Garcia on 24/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
