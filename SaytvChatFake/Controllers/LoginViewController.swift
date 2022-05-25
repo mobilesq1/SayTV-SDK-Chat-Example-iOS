aaaee80fea8d3a7cb049619866363958c03c3f9d
David Garcia Ruiz <david.garcia@square1.io>
2022-05-25 08:45:28 +0200

    Add login example

--- SaytvChatFake/Controllers/LoginViewController.swift
+++ SaytvChatFake/Controllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SaytvChat
 
 class LoginViewController: UIViewController {
 
@@ -17,7 +18,20 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func sendTapAction(_ sender: UIButton) {
-        
+        guard let text = digicelIdTextField.text,
+                let digicelId = Int(text)
+        else {
+            resultTextView.text = "DigicelId needs to be a number"
+            return
+        }
+        SayTvSdk.login(digicelId: digicelId) { result in
+            switch result {
+            case .success(let response):
+                self.resultTextView.text = "Success: \(response == true), User has login"
+            case .failure(let error):
+                self.resultTextView.text = "Failure: \(error.localizedDescription)"
+            }
+        }
     }
     
 }
