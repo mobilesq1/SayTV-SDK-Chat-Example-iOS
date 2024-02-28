a0e07ea4abd6a6e4fdafd988af39a19a952bf3f7
David Garcia Ruiz <david.garcia@square1.io>
2024-02-28 16:22:44 +0100

    Add initialize

--- SaytvChatMock/AppDelegate.swift
+++ SaytvChatMock/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        SayTvSdk.initialise(appName: "TEST APP", environment: .staging)
         SayTvSdk.setLogOption(.verbose)
         FirebaseApp.configure()
         
